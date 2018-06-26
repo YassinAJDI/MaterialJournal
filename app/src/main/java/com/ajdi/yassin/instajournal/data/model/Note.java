5f508d2f1c7c55bf43548a3a1c1fa6c628b96166
Yassin AJDI <yassin.ajdi@gmail.com>
2018-06-26 20:07:28 +0100

    Add note POJO

--- app/src/main/java/com/ajdi/yassin/instajournal/data/model/Note.java
+++ app/src/main/java/com/ajdi/yassin/instajournal/data/model/Note.java
@@ -1,4 +1,27 @@
 package com.ajdi.yassin.instajournal.data.model;
 
 public class Note {
+
+    private String mTitle;
+    private String mContent;
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        this.mTitle = title;
+    }
+
+    public String getContent() {
+        return mContent;
+    }
+
+    public void setContent(String content) {
+        this.mContent = content;
+    }
+
+    public String toString() {
+        return "{Note title=" + mTitle + " content=" + mContent + "}";
+    }
 }
