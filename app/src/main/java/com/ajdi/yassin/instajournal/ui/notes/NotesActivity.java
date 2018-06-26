0973d53d2b2b13c6d587413034d2682c7f33800e
Yassin AJDI <yassin.ajdi@gmail.com>
2018-06-26 22:11:18 +0100

    Add activity_notes layout

---
+++ app/src/main/java/com/ajdi/yassin/instajournal/ui/notes/NotesActivity.java
@@ -0,0 +1,18 @@
+package com.ajdi.yassin.instajournal.ui.notes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ajdi.yassin.instajournal.R;
+
+public class NotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes);
+
+
+
+    }
+}
