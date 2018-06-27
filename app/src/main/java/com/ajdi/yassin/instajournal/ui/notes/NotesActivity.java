a1184ca4295d7fbd217fd0ed9febf6b48701c670
Yassin AJDI <yassin.ajdi@gmail.com>
2018-06-27 01:02:04 +0100

    Setup toolbar

--- app/src/main/java/com/ajdi/yassin/instajournal/ui/notes/NotesActivity.java
+++ app/src/main/java/com/ajdi/yassin/instajournal/ui/notes/NotesActivity.java
@@ -1,7 +1,9 @@
 package com.ajdi.yassin.instajournal.ui.notes;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.ajdi.yassin.instajournal.R;
 
@@ -12,7 +14,14 @@ public class NotesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
 
+        setupToolbar();
 
+    }
 
+    private void setupToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null)
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
