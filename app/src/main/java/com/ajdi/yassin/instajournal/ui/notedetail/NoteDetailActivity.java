914f5fb55b6711a8f05d9504b66b1d56d872b3ed
Yassin AJDI <yassin.ajdi@gmail.com>
2018-06-27 21:16:09 +0100

    Setup notedetail activity

--- app/src/main/java/com/ajdi/yassin/instajournal/ui/notedetail/NoteDetailActivity.java
+++ app/src/main/java/com/ajdi/yassin/instajournal/ui/notedetail/NoteDetailActivity.java
@@ -3,15 +3,20 @@ package com.ajdi.yassin.instajournal.ui.notedetail;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import com.ajdi.yassin.instajournal.R;
+import com.ajdi.yassin.instajournal.ui.notes.NotesFragment;
 import com.ajdi.yassin.instajournal.ui.notes.NotesViewModel;
 import com.ajdi.yassin.instajournal.ui.notes.ViewModelFactory;
+import com.ajdi.yassin.instajournal.utils.ActivityUtils;
 
 public class NoteDetailActivity extends AppCompatActivity {
+
+    public static final String EXTRA_NOTE_ID = "TASK_ID";
 
     private NotesViewModel mViewModel;
 
@@ -31,7 +36,18 @@ public class NoteDetailActivity extends AppCompatActivity {
     }
 
     private void setupViewFragment() {
+        // Get the requested note id
+        String noteId = getIntent().getStringExtra(EXTRA_NOTE_ID);
 
+        NoteDetailFragment noteDetailFragment = (NoteDetailFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.fragment_container);
+
+        if (noteDetailFragment == null) {
+            noteDetailFragment = NoteDetailFragment.newInstance(noteId);
+        }
+
+        ActivityUtils.replaceFragmentInActivity(getSupportFragmentManager(),
+                noteDetailFragment, R.id.fragment_container);
     }
 
     private void setupToolbar() {
