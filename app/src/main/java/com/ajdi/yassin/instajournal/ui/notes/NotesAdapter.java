7f70fe060bc884d48d106a05b7aba5beee9e6ce3
Yassin AJDI <yassin.ajdi@gmail.com>
2018-06-27 03:28:58 +0100

    Setup NotesAdapter

--- app/src/main/java/com/ajdi/yassin/instajournal/ui/notes/NotesAdapter.java
+++ app/src/main/java/com/ajdi/yassin/instajournal/ui/notes/NotesAdapter.java
@@ -2,30 +2,42 @@ package com.ajdi.yassin.instajournal.ui.notes;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import com.ajdi.yassin.instajournal.data.model.Note;
+import com.ajdi.yassin.instajournal.databinding.ItemNoteBinding;
 
 import java.util.List;
 
 public class NotesAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
     private List<Note> mNotes;
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        ItemNoteBinding itemBinding =
+                ItemNoteBinding.inflate(layoutInflater, parent, false);
+        return new NoteViewHolder(itemBinding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+        Note note = mNotes.get(position);
+        NoteViewHolder noteViewHolder = (NoteViewHolder) holder;
+        noteViewHolder.bind(note);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mNotes != null ? mNotes.size() : 0;
+    }
+
+    public void replaceData(List<Note> notes) {
+        mNotes = notes;
+        notifyDataSetChanged();
     }
 
 
@@ -35,12 +47,17 @@ public class NotesAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
      */
     public class NoteViewHolder extends RecyclerView.ViewHolder {
 
-        //private final ItemNoteBin cardBinding;
+        private final ItemNoteBinding mBinding;
 
-        public NoteViewHolder(final View cardView) {
-            super(cardView);
+        public NoteViewHolder(final ItemNoteBinding binding) {
+            super(binding.getRoot());
 
-            //cardBinding = CardArticleItemBinding.bind(cardView);
+            mBinding = binding;
+        }
+
+        public void bind(Note note) {
+            //mBinding.setItem();
+            //binding.executePendingBindings();
         }
     }
 }
