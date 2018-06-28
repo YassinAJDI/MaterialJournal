212352b902bf39bbfeaa9355b47edf79d2a692fa
Yassin AJDI <yassin.ajdi@gmail.com>
2018-06-28 02:50:28 +0100

    Add note entity for room

--- app/src/main/java/com/ajdi/yassin/instajournal/data/model/Note.java
+++ app/src/main/java/com/ajdi/yassin/instajournal/data/model/Note.java
@@ -1,13 +1,35 @@
 package com.ajdi.yassin.instajournal.data.model;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "note")
 public class Note {
 
+    @PrimaryKey(autoGenerate = true)
+    @NonNull
+    @ColumnInfo(name = "id")
+    private String mId;
+
+    @ColumnInfo(name = "title")
     private String mTitle;
+
+    @ColumnInfo(name = "content")
     private String mContent;
 
-    public Note(String title, String content) {
-        mTitle = title;
-        mContent = content;
+    @ColumnInfo(name = "date")
+    private long mDate;
+
+    /**
+     * This constructor is used by Room.
+     */
+    public Note(@NonNull String id, String title, String content, long date) {
+        this.mId = id;
+        this.mTitle = title;
+        this.mContent = content;
+        this.mDate = date;
     }
 
     public String getTitle() {
