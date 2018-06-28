efb4ea8d830c6a9f77dd6774762a06b81629337f
Yassin AJDI <yassin.ajdi@gmail.com>
2018-06-28 01:40:30 +0100

    Obtain viewmodel

--- app/src/main/java/com/ajdi/yassin/instajournal/ui/addedit/AddEditNoteActivity.java
+++ app/src/main/java/com/ajdi/yassin/instajournal/ui/addedit/AddEditNoteActivity.java
@@ -1,16 +1,21 @@
 package com.ajdi.yassin.instajournal.ui.addedit;
 
+import android.arch.lifecycle.ViewModelProviders;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
 import com.ajdi.yassin.instajournal.R;
+import com.ajdi.yassin.instajournal.ui.notes.ViewModelFactory;
 
 /**
  * Displays an add or edit task screen.
  */
 public class AddEditNoteActivity extends AppCompatActivity {
+
+    AddEditNoteViewModel mViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +25,14 @@ public class AddEditNoteActivity extends AppCompatActivity {
 
         setupViewFragment();
 
-        //mViewModel = obtainViewModel(this);
+        mViewModel = obtainViewModel(this);
+    }
+
+    private AddEditNoteViewModel obtainViewModel(FragmentActivity activity) {
+        // Use a Factory to inject dependencies into the ViewModel
+        ViewModelFactory factory = ViewModelFactory.getInstance(activity.getApplication());
+
+        return ViewModelProviders.of(activity, factory).get(AddEditNoteViewModel.class);
     }
 
     private void setupViewFragment() {
