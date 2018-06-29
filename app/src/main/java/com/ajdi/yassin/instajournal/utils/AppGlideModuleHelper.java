daa8aa1e36274e17a4311d099d0c314aa956ea0d
Yassin AJDI <yassin.ajdi@gmail.com>
2018-06-29 11:29:53 +0100

    Add glide module

---
+++ app/src/main/java/com/ajdi/yassin/instajournal/utils/AppGlideModuleHelper.java
@@ -0,0 +1,31 @@
+package com.ajdi.yassin.instajournal.utils;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.request.RequestOptions;
+
+/**
+ * Ensures that Glide's generated API is created for this app.
+ *
+ * @author Yassin AJDI
+ */
+@GlideModule
+public class AppGlideModuleHelper extends AppGlideModule {
+
+    @Override
+    public boolean isManifestParsingEnabled() {
+        return false;
+    }
+
+    @Override
+    public void applyOptions(@NonNull Context context, @NonNull GlideBuilder builder) {
+        builder.setDefaultRequestOptions(
+                new RequestOptions()
+                        .placeholder(android.R.color.darker_gray)
+        );
+    }
+}
